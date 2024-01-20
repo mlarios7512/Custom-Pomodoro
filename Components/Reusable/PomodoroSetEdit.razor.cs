336e299403cb88a831233aa488a9693cc89a42c5
Michael Larios <lariosmichael98@gmail.com>
2024-01-20 13:35:56 -0800

    Added page & component (with code-behind) with form for creating a set,

---
+++ Components/Reusable/PomodoroSetEdit.razor.cs
@@ -0,0 +1,16 @@
+﻿using CustomPomodoro.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomPomodoro.Components.Reusable
+{
+    public partial class PomodoroSetEdit
+    {
+        [Parameter]
+        public PomoderoSet CurSet { get; set; }
+    }
+}
