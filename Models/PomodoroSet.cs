5b5bb989b3d9dad3480a2cd38b1b0dc88dbd2152
Michael Larios <lariosmichael98@gmail.com>
2024-01-20 12:18:01 -0800

    (Thought I added it before.) Added 'PomodoroSet' class.

---
+++ Models/PomodoroSet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomPomodoro.Models
+{
+    public class PomoderoSet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null;
+
+        public TimeSpan? ShortWorkTime = null;
+        public TimeSpan? ShortBreakTime = null;
+        public int? ShortSessionReps = null;
+
+        public TimeSpan? LongWorkTime = null;
+        public TimeSpan? LongBreakTime = null;
+    }
+}
