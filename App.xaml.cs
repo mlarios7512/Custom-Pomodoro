633fa094267a9ce57cb956e45696db76f8585589
Michael Larios <lariosmichael98@gmail.com>
2024-01-20 12:00:39 -0800

    Added project initialization files & 'PomodoroSet' class.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CustomPomodoro
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
