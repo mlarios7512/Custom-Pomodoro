b59a1d55b92579ec584f347c8a07fc545c797505
Michael Larios <lariosmichael98@gmail.com>
2024-01-25 13:14:08 -0800

    Renamed home page to 'Timer'. Added code behind.

---
+++ Components/Pages/Timer.razor.cs
@@ -0,0 +1,24 @@
+﻿using CustomPomodoro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomPomodoro.Components.Pages
+{
+    public partial class Timer
+    {
+        public PomoderoSet PomoderoSet { get; set; }
+        public int SessionCount { get; set; }
+
+        public TimeSpan GetEndTime() 
+        {
+            string[] WorkTime =  PomoderoSet.WorkTime.Split(':');
+
+            const int SECONDS_PER_MINUTE = 60;
+            int WorkTimeInTotalSeconds = (int.Parse(WorkTime[0]) * SECONDS_PER_MINUTE) + int.Parse(WorkTime[1]);
+            return TimeSpan.FromSeconds(WorkTimeInTotalSeconds);
+        }
+    }
+}
