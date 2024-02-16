2602f5d42b2dac4509547b9bf7680610ac3df651
Michael Larios <lariosmichael98@gmail.com>
2024-02-15 19:30:31 -0800

    Re-wrote timer logic. Timer now visibly updates on page.

--- Components/Pages/Timer.razor.cs
+++ Components/Pages/Timer.razor.cs
@@ -1,6 +1,8 @@
-﻿using CustomPomodoro.Models;
+﻿//using Android.OS;
+using CustomPomodoro.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,43 +11,59 @@ namespace CustomPomodoro.Components.Pages
 {
     public partial class Timer
     {
-        public enum TimerMode
-        {
-            NotStarted = -1,
-            Paused = 0,
-            Started = 1
-        }
+
+        //protected override async Task OnInitializedAsync() 
+        //{ 
+            
+        //}
+
 
         //The "new ()" part of the statement below is for testing purposes ONLY. (Real data will be loaded from local machine.)
         public PomoderoSet CurPomodoroSet { get; set; } = new();
-        public TimerMode MainTimerState { get; set; } = TimerMode.NotStarted;
         public string PomodoroWorkState { get; set; } = "Work";  //"Work", "Short Break", or "Long Break".
-        public TimeSpan VisibleTimerDisplay { get; set; }
+        public int TimerInSeconds { get; set; } = -1;
+        public System.Timers.Timer ActualCountdownTimer { get; set; } = new();
         public int SessionCount { get; set; } = 0;
 
-        public TimeSpan GetEndTimeInSecondsFormat(string workTimeInput)
+        public int GetEndTimeInSecondsFormat(string workTimeInput)
         {
             string[] workTimeAsArray =  CurPomodoroSet.WorkTime.Split(':');
 
             const int SECONDS_PER_MINUTE = 60;
             int WorkTimeInTotalSeconds = (int.Parse(workTimeAsArray[0]) * SECONDS_PER_MINUTE) + int.Parse(workTimeAsArray[1]);
-            return TimeSpan.FromSeconds(WorkTimeInTotalSeconds);
+            return WorkTimeInTotalSeconds;
         }
 
         public async Task StartTimer(PomoderoSet PomoSet)
         {
             //Start timer countdown after obtaining it.
-            VisibleTimerDisplay =  GetEndTimeInSecondsFormat(PomoSet.WorkTime);
+            TimerInSeconds = GetEndTimeInSecondsFormat(PomoSet.WorkTime);
 
             //To do: Create logic for starting a timer.
             SessionCount++;
-            MainTimerState = TimerMode.Started;
 
-            //while (VisibleTimerDisplay > TimeSpan.Zero)
-            //{
-            //    VisibleTimerDisplay.Subtract(TimeSpan.FromSeconds(1));
-            //}
 
+            ActualCountdownTimer = new System.Timers.Timer(1000); //5 Sec timer.
+            ActualCountdownTimer.Enabled = true;
+            ActualCountdownTimer.Elapsed += CountDownTimer;
+        }
+
+        public string PrintCountdownTimer() 
+        {
+            return $"{TimeSpan.FromSeconds(TimerInSeconds):hh\\:mm\\:ss}";
+        }
+
+        public void CountDownTimer(Object source, System.Timers.ElapsedEventArgs e) 
+        {
+            if(TimerInSeconds > 0) 
+            {
+                TimerInSeconds--;
+            }
+            else 
+            {
+                ActualCountdownTimer.Enabled = false;   
+            }
+            InvokeAsync(StateHasChanged);
         }
     }
 }
