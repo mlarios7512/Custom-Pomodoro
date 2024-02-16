8dbb500a9e0a6c8c2c78f892f160d8ae1dc90670
Michael Larios <lariosmichael98@gmail.com>
2024-02-15 17:13:49 -0800

    Added logic for displaying a pomodoro (practice) timer.

--- Components/Pages/Timer.razor.cs
+++ Components/Pages/Timer.razor.cs
@@ -9,16 +9,38 @@ namespace CustomPomodoro.Components.Pages
 {
     public partial class Timer
     {
-        public PomoderoSet PomoderoSet { get; set; }
-        public int SessionCount { get; set; }
+        //The "new ()" part of the statement below is for testing purposes ONLY. (Real data will be loaded from local machine.)
+        public PomoderoSet CurPomodoroSet { get; set; } = new();
+        public TimerMode MainTimerState { get; set; } = TimerMode.NotStarted;
+        public TimeSpan VisibleTimerDisplay { get; set; }
+        public int SessionCount { get; set; } = 0;
 
-        public TimeSpan GetEndTime() 
+        public TimeSpan GetEndTimeInSecondsFormat(string workTimeInput)
         {
-            string[] WorkTime =  PomoderoSet.WorkTime.Split(':');
+            string[] workTimeAsArray =  CurPomodoroSet.WorkTime.Split(':');
 
             const int SECONDS_PER_MINUTE = 60;
-            int WorkTimeInTotalSeconds = (int.Parse(WorkTime[0]) * SECONDS_PER_MINUTE) + int.Parse(WorkTime[1]);
+            int WorkTimeInTotalSeconds = (int.Parse(workTimeAsArray[0]) * SECONDS_PER_MINUTE) + int.Parse(workTimeAsArray[1]);
             return TimeSpan.FromSeconds(WorkTimeInTotalSeconds);
+        }
+
+        public enum TimerMode 
+        {
+            NotStarted = -1,
+            Paused = 0,
+            Started = 1
+        }
+
+        public TimerMode StartTimer(PomoderoSet PomoSet)
+        {
+            //Start timer countdown after obtaining it.
+            TimeSpan Timer =  GetEndTimeInSecondsFormat(PomoSet.WorkTime);
+
+            //To do: Create logic for starting a timer.
+
+
+            SessionCount++;
+            return TimerMode.Started;
         }
     }
 }
