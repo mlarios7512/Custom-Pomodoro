1d55022fc04283ebfc384bb7cbde881fb12223b9
Michael Larios <lariosmichael98@gmail.com>
2024-04-29 13:55:16 -0700

    Added class to retrieve the proper background-color based on current activity.

---
+++ Models/Helpers/PomTimer/HslColorSelection.cs
@@ -0,0 +1,61 @@
+﻿using CustomPomodoro.Models.UserSettings;
+using Microsoft.Maui.ApplicationModel.Communication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomPomodoro.Models.Helpers.PomTimer
+{
+    internal class HslColorSelection
+    {
+        private static readonly HSLColor DefaultNoActivityBgColor = new HSLColor(240, 4, 46);
+        private static readonly HSLColor DefaultActivityInProgressBgColor = new HSLColor(240, 6, 10);
+        private static readonly HSLColor DefaultPausedActivityBgColor = new HSLColor(202, 96, 22);
+
+
+        public static string GetNoActivityHLSColor(HSLColor? color = null)
+        {
+
+            if (color == null) 
+            {
+                return $"hsl({DefaultNoActivityBgColor.Hue}, " +
+                    $"{DefaultNoActivityBgColor.Saturation}, " +
+                    $"{DefaultNoActivityBgColor.Lightness})";
+            }
+
+            return $"hsl({color.Hue}" +
+                $"{color.Saturation}" +
+                $"{color.Lightness})";
+        }
+        public static string GetActivityInProgressHSLColor(HSLColor? color = null)
+        {
+            if(color == null) 
+            {
+                return $"{DefaultActivityInProgressBgColor.Hue}" +
+                    $"{DefaultActivityInProgressBgColor.Saturation}" +
+                    $"{DefaultActivityInProgressBgColor.Lightness})";
+            }
+
+            return $"hsl({color.Hue}" +
+                $"{color.Saturation}" +
+                $"{color.Lightness})";
+        }
+
+        public static string GetPausedActivity(HSLColor? color = null) 
+        {
+            if(color == null) 
+            {
+                return $"hsl({DefaultPausedActivityBgColor.Hue}" +
+                    $"{DefaultPausedActivityBgColor.Saturation}" +
+                    $"{DefaultPausedActivityBgColor.Lightness})";
+            }
+
+            return $"hsl({color.Hue}" +
+                $"{color.Saturation}" +
+                $"{color.Lightness})";
+        }
+    }
+
+}
