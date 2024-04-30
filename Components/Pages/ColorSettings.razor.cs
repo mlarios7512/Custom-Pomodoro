cb12a3fe571baa8ca5bc0f4ae2b53d77740777d2
Michael Larios <lariosmichael98@gmail.com>
2024-04-30 16:03:18 -0700

    Started work on form that will allow user to change 'activity status bar' colors.

---
+++ Components/Pages/ColorSettings.razor.cs
@@ -0,0 +1,20 @@
+﻿using CustomPomodoro.Models.UserSettings;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomPomodoro.Components.Pages
+{
+    public partial class ColorSettings
+    {
+        ActivityBarSettings BarSettings { get; set; } = new ActivityBarSettings();
+
+        public void Cha() 
+        {
+            Debug.WriteLine($"{BarSettings.WorkColors.First().Hue}");
+        }
+    }
+}
