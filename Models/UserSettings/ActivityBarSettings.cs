c27e1fe8ed17ae744ae05f42878c36d69362de8b
Michael Larios <lariosmichael98@gmail.com>
2024-04-30 16:00:01 -0700

    Added 'ActivityBarSettings' class (that will be used to change/save some user settings later.)

---
+++ Models/UserSettings/ActivityBarSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomPomodoro.Models.UserSettings
+{
+    public class ActivityBarSettings
+    {
+        public List<HSLColor> WorkColors { get; set; } = new List<HSLColor>() { new HSLColor(0,0,0), new HSLColor(0, 0, 0) };
+        public List<HSLColor> ShortBreakColors { get; set; } = new List<HSLColor>() { new HSLColor(0,0,0), new HSLColor(0,0,0) };
+        public List<HSLColor> LongBreakColors { get; set; } = new List<HSLColor>() { new HSLColor(0,0,0), new HSLColor(0, 0, 0) };
+
+
+        public ActivityBarSettings()
+        {
+            
+        }
+        public void Save() 
+        {
+            
+        }
+
+        public void Load() 
+        {
+            
+        }
+        public void Update() 
+        {
+
+        }
+    }
+}
