cd6751e4c410a9dd627ed41e23b192b200235fa8
Michael Larios <lariosmichael98@gmail.com>
2024-05-03 12:17:12 -0700

    Added file to track user preference of bg colors for timer.

---
+++ Models/UserSettings/BackgroundColorSettings.cs
@@ -0,0 +1,17 @@
+﻿using CustomPomodoro.Models.Helpers.PomTimer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomPomodoro.Models.UserSettings
+{
+    public class BackgroundColorSettings
+    {
+        public HSLColor NoActivityBgColor { get; set; } = HslColorSelection.GetDefaultNoActivityBgColor();
+        public HSLColor ActivityInProgressColor { get; set; } = HslColorSelection.GetDefaultActivityInProgressBgColor();
+        public HSLColor PausedActivityColor { get; set; } = HslColorSelection.GetDefaultPausedActivityBgColor();
+        public BackgroundColorSettings() { }
+    }
+}
