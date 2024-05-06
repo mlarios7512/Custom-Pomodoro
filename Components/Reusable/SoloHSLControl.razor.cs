08e591ecaf813cf7dc2f7772691cabb2f02d1cc6
Michael Larios <lariosmichael98@gmail.com>
2024-05-06 14:11:25 -0700

    Added solo version of HSLControl for bgColor input.

---
+++ Components/Reusable/SoloHSLControl.razor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+using CustomPomodoro.Models;
+
+namespace CustomPomodoro.Components.Reusable
+{
+    public partial class SoloHSLControl
+    {
+        [Parameter]
+        public HSLColor Color { get; set; }
+
+        [Parameter]
+        public string Header { get; set; } = string.Empty;
+    }
+}
