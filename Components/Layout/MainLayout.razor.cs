db2e3264eec3d6f9fd420177a7dd0a86c1722bda
Michael Larios <lariosmichael98@gmail.com>
2024-05-23 16:57:53 -0700

    Accidently forgot to this part include of 'MasterUserSettings' (should've been in last commit).

--- Components/Layout/MainLayout.razor.cs
+++ Components/Layout/MainLayout.razor.cs
@@ -1,4 +1,5 @@
 ﻿using CustomPomodoro.Models;
+using CustomPomodoro.Models.UserSettings.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -11,7 +12,12 @@ namespace CustomPomodoro.Components.Layout
     public partial class MainLayout
     {
         //Set up using this link: https://stackoverflow.com/questions/71713761/how-can-i-declare-a-global-variables-model-in-blazor -- Daniël J.M. Hoffman
-        public static PomodoroSet CurPomodoroSet { get; set; } = new();
-        
+        public static CustomPomodoro.Models.UserSettings.Concrete.MasterUserSettings UserSettings { get; set; } = MasterUserSettings.GetSingletonInstance();
+        protected override async Task OnInitializedAsync() 
+        {
+            UserSettings.LoadAllSettings();
+        }
+
+
     }
 }
