e6f19fd99361f0d0394eb16181cb2123fcb73622
Michael Larios <lariosmichael98@gmail.com>
2024-07-09 11:02:10 -0700

    Added view-model & TimerSettings class (for saving data in 'ChangeTimer' page).

---
+++ Models/UserSettings/Concrete/PomodoroTimerSettings.cs
@@ -0,0 +1,14 @@
+﻿using CustomPomodoro.ViewModels.InputClones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomPomodoro.Models.UserSettings.Concrete
+{
+    public class PomodoroTimerSettings
+    {
+        public PomodoroSet StoredPomSet { get; set; } = new();
+    }
+}
---
+++ ViewModels/Pages/ChangeTimer/ChangeTimerVM.cs
@@ -0,0 +1,35 @@
+﻿using CustomPomodoro.Models;
+using CustomPomodoro.Models.Helpers;
+using CustomPomodoro.ViewModels.InputClones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomPomodoro.ViewModels.Pages.ChangeTimer
+{
+    public partial class ChangeTimerVM
+    {
+        public PomTimerSetInputClone PomSetInput { get; set; } = new();
+
+        public static void DummyPomSetInputToRealPomSet(PomTimerSetInputClone userInput, ref PomodoroSet pomSet)
+        {
+            //int totalSeconds = userInput.WT_Seconds  Integer.To;
+
+            int workTimeInSeconds = Int32.Parse(userInput.WT_Minutes) * 60;
+            workTimeInSeconds += Int32.Parse(userInput.WT_Seconds);
+            pomSet.WorkTime = PomTimerHelpers.PrintCountdownTimer(workTimeInSeconds);
+
+            int shortBreakInSeconds = Int32.Parse(userInput.SBT_Minutes) * 60;
+            shortBreakInSeconds += Int32.Parse(userInput.SBT_Seconds);
+            pomSet.ShortBreak = PomTimerHelpers.PrintCountdownTimer(shortBreakInSeconds);
+
+            int longBreakInSeconds = Int32.Parse(userInput.LBT_Seconds) * 60;
+            longBreakInSeconds += Int32.Parse(userInput.LBT_Seconds);
+            pomSet.LongBreak = PomTimerHelpers.PrintCountdownTimer(longBreakInSeconds);
+
+            pomSet.RepsBeforeLongBreak = userInput.RepsBeforeLongBreak;
+        }
+    }
+}
