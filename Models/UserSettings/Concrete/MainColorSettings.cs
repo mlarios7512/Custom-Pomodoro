440850306acbae75c8cc127999494bcc12903580
Michael Larios <lariosmichael98@gmail.com>
2024-07-27 14:14:00 -0700

    Added POCO object for cleaner json serialization of color settings.

---
+++ Models/UserSettings/Concrete/MainColorSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomPomodoro.Models.UserSettings.Concrete
+{
+    //This is intended as a POCO to serialize JSON data. Do NOT uses interfaces for this (or any of it's child classes).
+    public class MainColorSettings
+    {
+        public ActivityBarSettings ActivityColorSettings { get; set; } = new();
+        public BackgroundColorSettings BackgroundColorSettings { get; set; } = new();
+    }
+}
