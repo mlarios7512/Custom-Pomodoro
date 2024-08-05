c0787b923d0e6cffebd53c6e211888f51b141ebf
Michael Larios <lariosmichael98@gmail.com>
2024-08-05 13:37:24 -0700

    Removed unused duplicate functions.

--- Models/Helpers/BusinessLogic (Static functions)/PomTimer/HslColorSelection.cs
+++ Models/Helpers/BusinessLogic (Static functions)/PomTimer/HslColorSelection.cs
@@ -43,8 +43,6 @@ namespace CustomPomodoro.Models.Helpers.BusinessLogic.PomTimer
             return FormatHSLToCSS(color);
         }
 
-        /*----------------*/
-
         public static HSLColor GetDefaultNoActivityBgColor()
         {
             return new HSLColor(0, 0, 0);
@@ -57,56 +55,6 @@ namespace CustomPomodoro.Models.Helpers.BusinessLogic.PomTimer
         public static HSLColor GetDefaultPausedActivityBgColor()
         {
             return new HSLColor(207, 100, 26);
-        }
-
-
-
-        private const int BarActivityStatusSaturation = 99;
-        private const int BarActivityStatusLightnessDim = 27;
-        private const int BarActivityStatusLightnessBright = 44;
-
-        private static (string, string) GetDefaultOrUserDefinedBarColors(int adminDefaultColor, HSLColor? colorOne, HSLColor? colorTwo)
-        {
-            HSLColor DefaultStatusColorBright = new HSLColor(
-               adminDefaultColor,
-               BarActivityStatusSaturation,
-               BarActivityStatusLightnessBright);
-
-            HSLColor DefaultStatusColorDim = new HSLColor(
-                adminDefaultColor,
-                BarActivityStatusSaturation,
-                BarActivityStatusLightnessDim);
-
-            string colorOneAsHSL = string.Empty;
-            string colorTwoAsHSL = string.Empty;
-            if (colorOne == null)
-                colorOneAsHSL = FormatHSLToCSS(DefaultStatusColorBright);
-            else
-                colorOneAsHSL = FormatHSLToCSS(colorOne);
-
-            if (colorTwo == null)
-                colorTwoAsHSL = FormatHSLToCSS(DefaultStatusColorDim);
-            else
-                colorTwoAsHSL = FormatHSLToCSS(colorTwo);
-
-            return (colorOneAsHSL, colorTwoAsHSL);
-        }
-
-        //If we decide to limit the HSL color options for "activity status bar", we do it from the
-        //input form's backend (to avoid limiting potential reuse).
-        public static (string, string) GetWorkStatusBarColor(HSLColor? colorOne = null, HSLColor? colorTwo = null)
-        {
-            return GetDefaultOrUserDefinedBarColors(0, colorOne, colorTwo);
-        }
-
-        public static (string, string) GetShortBreakStatusBarColor(HSLColor? colorOne = null, HSLColor? colorTwo = null)
-        {
-            return GetDefaultOrUserDefinedBarColors(191, colorOne, colorTwo);
-        }
-
-        public static (string, string) GetLongBreakStatusBarColor(HSLColor? colorOne = null, HSLColor? colorTwo = null)
-        {
-            return GetDefaultOrUserDefinedBarColors(121, colorOne, colorTwo);
         }
     }
 
