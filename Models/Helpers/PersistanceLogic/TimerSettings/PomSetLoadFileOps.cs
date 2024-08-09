3b96dfc26132418bc35c25ecf386806fed2cef16
Michael Larios <lariosmichael98@gmail.com>
2024-08-09 13:25:21 -0700

    Changed location of save data.

--- Models/Helpers/PersistanceLogic/TimerSettings/PomSetLoadFileOps.cs
+++ Models/Helpers/PersistanceLogic/TimerSettings/PomSetLoadFileOps.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,22 +15,32 @@ namespace CustomPomodoro.Models.Helpers.PersistanceLogic.TimerSettings
 
         public static Task LoadCurrentPomodoroSetFromFile(ref PomodoroTimerSettings pomSet)
         {
-            string JsonSavePath = Path.Combine(AppContext.BaseDirectory, _saveFileName);
+            string JsonSavePath = Path.Combine(FileSystem.Current.AppDataDirectory, _saveFileName);
 
-            if (File.Exists(JsonSavePath)) 
+            try
             {
-                string PomSetFromJsonFile = File.ReadAllText(JsonSavePath);
-                pomSet = JsonConvert.DeserializeObject<PomodoroTimerSettings>(PomSetFromJsonFile);
+                if (File.Exists(JsonSavePath))
+                {
+                    string PomSetFromJsonFile = File.ReadAllText(JsonSavePath);
+                    pomSet = JsonConvert.DeserializeObject<PomodoroTimerSettings>(PomSetFromJsonFile);
 
-                if (pomSet == null) 
+                    if (pomSet == null)
+                        pomSet = new PomodoroTimerSettings();
+
+
+                }
+                else if (pomSet == null)
+                {
                     pomSet = new PomodoroTimerSettings();
-                
-                
+                }
             }
-            else if(pomSet == null)
+            catch (Exception ex) 
             {
-                pomSet = new PomodoroTimerSettings();
+                Debug.WriteLine($"An error occured when loading the pomodoro set from file. INFO: {ex}");
+                if(pomSet == null)
+                    pomSet= new PomodoroTimerSettings();
             }
+
             return Task.FromResult(pomSet);
         }
     }
